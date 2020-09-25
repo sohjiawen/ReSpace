6cc3c60cf6442d65778ac4cc361f59f84f002789
Florence Loy <florencelry@gmail.com>
2020-09-25 15:32:33 +0800

    modified links

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,13 +27,10 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-import { init, animate } from '../plugins/initAR'
 import { themes } from '../plugins/initThemeImages'
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  init();
-  animate();
   themes();
 });
