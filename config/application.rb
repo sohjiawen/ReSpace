6199ebb32cc5f18d5529153da69565a6cab09ca3
Florence Loy <florencelry@gmail.com>
2020-09-25 16:19:35 +0800

    some mods

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Respace
   class Application < Rails::Application
+    config.web_console.whitelisted_ips = '116.12.176.90'
+
     config.generators do |generate|
       generate.assets false
       generate.helper false
