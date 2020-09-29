962fd05ac81bc35dbae1716f904d1cb106854edc
yasmin dyana <yasmindyana@gmail.com>
2020-09-29 16:36:42 +0800

    fix migrations

--- db/migrate/20200929014550_drop_carts.rb
+++
@@ -1,5 +0,0 @@
-class DropCarts < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :carts
-  end
-end
--- db/migrate/20200929033512_remove_refences_add_references.rb
+++ db/migrate/20200929033512_remove_refences_add_references.rb
@@ -1,6 +1,5 @@
 class RemoveRefencesAddReferences < ActiveRecord::Migration[6.0]
   def change
     remove_column :cart_items, :user_id
-    add_reference :cart_items, :cart, foreign_key: true
   end
 end
