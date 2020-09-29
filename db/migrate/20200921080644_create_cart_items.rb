9ecc0fb90d062f0320d9f20afcff03704ef0d841
yasmin dyana <yasmindyana@gmail.com>
2020-09-29 16:57:26 +0800

    fix migration

--- db/migrate/20200921080644_create_cart_items.rb
+++ db/migrate/20200921080644_create_cart_items.rb
@@ -3,7 +3,7 @@ class CreateCartItems < ActiveRecord::Migration[6.0]
     create_table :cart_items do |t|
       t.references :cart, null: false, foreign_key: true
       t.references :furniture, null: false, foreign_key: true
-      t.reference :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20200929015647_add_user_id_to_cart_items.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToCartItems < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :cart_items, :user, foreign_key: true
-  end
-end
