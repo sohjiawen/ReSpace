ccfb26d1e80d793735cb0fb18165bd076eee7115
Florence Loy <florencelry@gmail.com>
2020-09-29 15:36:42 +0800

    search and theme filter done

--- app/controllers/furnitures_controller.rb
+++ app/controllers/furnitures_controller.rb
@@ -1,6 +1,16 @@
 class FurnituresController < ApplicationController
   def index
-    @furnitures = Furniture.all
+    if params[:theme_name]
+      Theme::THEMES.each do |theme|
+        @theme = Theme.find_by(name: params[:theme_name])
+        @furnitures = Furniture.where(theme_id: @theme.id)
+      end
+    elsif params[:query].present?
+      sql_query = "name ILIKE :query"
+      @furnitures = Furniture.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @furnitures = Furniture.all
+    end
   end
 
   def show
