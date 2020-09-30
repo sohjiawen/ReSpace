aa15aa9e07da6a36bd0abd132ba202300586dd4e
Jiawen Soh <sohjiawen@gmail.com>
2020-09-30 13:00:28 +0800

    page items aligned

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,6 +1,7 @@
 class PaymentsController < ApplicationController
   def new
     @order = current_user.cart.orders.where(state: 'pending').find(params[:order_id])
+    @cart_items = current_user.cart_items.where(purchased: false)
 
   end
 end
